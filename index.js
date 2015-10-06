7f1d3a282c9d4399bcdac2f130f04b7ebbdd9af9
Paul James <p.james@coolblue.nl>
2015-10-06 17:55:41 +0200

    Register halacious with the server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var Hapi = require("hapi");
+var halacious = require("halacious");
 var fs = require("fs");
 
 var server = new Hapi.Server();
@@ -8,6 +9,22 @@ var server = new Hapi.Server();
 server.connection({
   host: "localhost",
   port: 8000
+});
+
+server.register(halacious, function registerHalacious(error) {
+  if (error) {
+    return console.log(error);
+  }
+
+  var ns = server.plugins.halacious.namespaces.add({
+    name: "undefined",
+    description: "Hackathon",
+    prefix: "hack"
+  });
+
+  ns.rel({ name: "qaid", description: "A collection of questions" });
+  ns.rel({ name: "question", description: "A question" });
+  ns.rel({ name: "answer", description: "An answer" });
 });
 
 fs.readdir("./lib/resources/", function getFiles(error, files) {
