8c933ee16cf9bdfc2772f6dee3080892b087e434
Paul James <p.james@coolblue.nl>
2015-10-06 16:11:07 +0200

    Added empty resources

--- index.js
+++ index.js
@@ -6,20 +6,26 @@ var fs = require("fs");
 var server = new Hapi.Server();
 
 server.connection({
-    host: 'localhost',
-    port: 8000
+  host: "localhost",
+  port: 8000
 });
 
-fs.readdir("./lib/resources/", function getFiles(error, files){
-  if(error) throw error;
+fs.readdir("./lib/resources/", function getFiles(error, files) {
+  if (error) {
+    throw error;
+  }
+
   files.forEach(function getFile(file) {
-    if(file.indexOf(".js") < 0) return;
-    server.route(require("./lib/resources/"+file));
+    if (file.indexOf(".js") < 0) {
+      return;
+    }
+
+    server.route(require("./lib/resources/" + file));
   });
 
 });
 
 // Start the server
-server.start(function () {
+server.start(function start() {
   console.log("Server running at:", server.info.uri);
 });
