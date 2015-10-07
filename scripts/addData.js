1b3b95b619053c9d8fb25793dfd9974b0483aeef
Emre Turan <coolblue@Coolblues-MBP.hackathon.lan>
2015-10-07 12:23:31 +0200

    randomized data

--- scripts/addData.js
+++ scripts/addData.js
@@ -1,8 +1,11 @@
+var faker = require("faker");
 var MongoDbConnection = require("../lib/services/mongoDb");
 var QAServvie = require("../lib/services/QAService");
 var objectId = require("mongoose").Types.ObjectId;
 
 var data;
+
+data = generateData();
 
 init();
 
@@ -27,93 +30,34 @@ function init() {
   });
 }
 
-data = [{
-  approved: true,
-  questions: [{
-    question: "How are you?",
-    _id: new objectId(),
-    approved: true,
-    answers: [{
-      answer: "fucked up!",
-      _id: new objectId(),
-    }, {
-      answer: "sleepy",
-      _id: new objectId(),
-      approved: true
-    }, {
-      answer: "it is not your problem",
-      _id: new objectId(),
-      approved: true
-    }, {
-      answer: "fine thank you!",
-      _id: new objectId(),
-      approved: true
-    }]
-  }, {
-    question: "Is this product the best one",
-    _id: new objectId(),
-    answers: [{
-      answer: "probably",
-      _id: new objectId(),
-    }, {
-      answer: "of course no!",
-      _id: new objectId(),
-    }]
-  }]
-}, {
-  questions: [{
-    question: "How are you?",
-    _id: new objectId(),
-    answers: [{
-      answer: "fucked up!",
-      _id: new objectId(),
-    }, {
-      answer: "sleepy",
-      _id: new objectId(),
-    }, {
-      answer: "it is not your problem",
-      _id: new objectId(),
-    }, {
-      answer: "fine thank you!",
-      _id: new objectId(),
-    }]
-  }, {
-    question: "when did you get it at home",
-    approved: true,
-    _id: new objectId(),
-    answers: [{
-      answer: "5 min ago!",
-      approved: true,
+function generateData() {
+  var d = [];
 
-    }, {
-      answer: "I ate it!",
-      approved: false
-    }]
-  }]
-}, {
-  approved: true,
-  questions: [{
-    question: "I broke my knee, should I buy this?",
-    approved: true,
-    answers: [{
-      answer: "first fix your knee!",
-      approved: true
-    }, {
-      answer: "yes it is the best one",
-      _id: new objectId(),
-      approved: true
-    }, {
-      answer: "I don't care",
-      _id: new objectId(),
-      approved: true
-    }, {
-      answer: "fine thank you!",
-      _id: new objectId(),
-      approved: true
-    }]
-  }, {
-    question: "I can fly, can you?",
-    _id: new objectId(),
-    approved: false
-  }]
-}];
+  console.log("generating random questions...");
+
+  for (var i = 0; i < faker.random.number(10); i++) {
+    d.push({
+      approved: true,
+      questions: []
+    });
+
+    for (var q = 0; q < faker.random.number(5); q++) {
+      d[i].questions.push({
+        question: faker.lorem.sentence(),
+        _id: new objectId(),
+        approved: true,
+        answers: []
+      });
+
+      for (var a = 0; a < faker.random.number(7); a++) {
+        d[i].questions[q].answers.push({
+          answer: faker.lorem.sentence(),
+          _id: new objectId(),
+          approved: true
+        });
+      }
+    }
+  }
+
+  return d;
+}
